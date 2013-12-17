d5fd416b4c0443bff6e97b4ca3283e9da46466f3
matiya <matymador@gmail.com>
2013-12-17 17:34:18 +0100

    added parameter parsing capabilities

--- mobilityGenerator/main.cpp
+++ mobilityGenerator/main.cpp
@@ -4,7 +4,7 @@
 #include <random>
 #include "node.h" 
 #include "parameters.h"
-#include "argvparser.h"
+
 
 int random_integer(std::mt19937& generator, int n){
     /*returns a random integer between 0 and n*/
@@ -13,31 +13,36 @@ int random_integer(std::mt19937& generator, int n){
 }
 
 double normal_integer(std::default_random_engine& generator, double mean, double stdDeviation){
-
-  //std::default_random_engine generator;
+  /*returns a random double drawn from a normal distribution*/
   std::normal_distribution<double> distribution(mean, stdDeviation);
   return std::abs(distribution(generator));
 }
-using namespace CommandLineProcessing;
+
+
+
 int main(int argc, char **argv) {
   
-  ArgvParser parser;
-//   parser.defineOption("first_long");
-//   parser.defineOption("f", "", CommandLineProcessing::ArgvParser::OptionRequired);
-//   parser.defineOption("d", "", CommandLineProcessing::ArgvParser::OptionRequired);
-//   if (parser.parse(argc, argv) != CommandLineProcessing::ArgvParser::ParserRequiredOptionMissing)
-//     std::cout << "nothing" << std::endl;
-   
+  askForParameters(argc, argv); 
   
+  int k=0;
   double time;
   double prevPosYFF, prevPosXFF, prevPosXD, prevPosYD, yPos;
   double yDeviation, xDeviation, xDir, yDir;
   double dogSpeed;
   const double rfDist = (double) FIELD_SIZE_X / NUM_NODES;
-  const double timeStep = (double) DURATION/SAMPLES_NUMBER;  //std::cout << "timestep: " << timeStep << std::endl;
+  const double timeStep = (double) DURATION/SAMPLES_NUMBER;  
   std::ofstream resultsFile, nsFile;
-  nsFile.open(FILEPATH1, std::ofstream::out | std::ofstream::trunc); //to feed into ns-3
-  resultsFile.open(FILEPATH, std::ofstream::out | std::ofstream::trunc); //for debugging purposes
+  std::ifstream f(FILE_NAME);
+  
+  //check if file exists already
+  while (f.good()) {
+    k++;
+    f.close();
+    f.open(FILE_NAME + std::to_string(k));
+  }
+  
+  nsFile.open("ns_" + FILE_NAME + std::to_string(k), std::ofstream::out | std::ofstream::trunc); //to feed into ns-3
+  resultsFile.open(FILE_NAME + std::to_string(k), std::ofstream::out | std::ofstream::trunc); //for debugging purposes
 
   std::random_device rd;
   
@@ -45,8 +50,6 @@ int main(int argc, char **argv) {
   SEED == 0 ? seedForGen = rd() : seedForGen = SEED;
   std::mt19937 gen(seedForGen);
   std::default_random_engine gen2(rd());
-  
-  
   
   Node firefighter[NUM_NODES], dog[NUM_NODES]; 
   
@@ -57,12 +60,12 @@ int main(int argc, char **argv) {
     dog[i].setPosition(0, DELTA_X + i*rfDist, 0, SPEED_NODES);
     dog[i].setSpeed(DOG_SPEED);
   }
-  
+  std::cout << "timestep: " << timeStep << std::endl;
   //update nodes positions
   while(time < DURATION){
     
     time += timeStep;
-    for(int i = 0; i<NUM_NODES; i++){
+    for(int i = 0; i<NUM_NODES; i++){      
       //load position at t-1 from the corresponding node's vectorPosition
       try{
 	prevPosYFF = *(firefighter[i].getPosition().end()-2);
@@ -72,9 +75,9 @@ int main(int argc, char **argv) {
 	dogSpeed = normal_integer(gen2, DOG_SPEED, STD_DEVIATION);
       }
       catch(std::exception& e){
-	//std::cout << "ERROR: accesing previous positions" << std::endl;
+	std::cout << "ERROR: accesing previous positions" << std::endl;
       }
-      
+
       //update position of firefighters
       firefighter[i].setPosition(time, prevPosXFF, prevPosYFF + timeStep*SPEED_NODES, SPEED_NODES);
       
@@ -82,9 +85,10 @@ int main(int argc, char **argv) {
       if(!dog[i].isAstray){
 	if(random_integer(gen, 1000) < PROB){
 	  dog[i].isAstray = true;
-	  std::cout << "@" << time << "s dog[" << i << "] has gone astray" << std::endl;
-	  std::cout << "current pos: ("<< prevPosXD <<"," << prevPosYD << ")" << std::endl;
-	  
+	  if(verbose){ 
+	    std::cout << "@" << time << "s dog[" << i << "] has gone astray" << std::endl;
+	    std::cout << "current pos: ("<< prevPosXD <<"," << prevPosYD << ")" << std::endl;
+	  }
 	  do{ //calculate new random position, making sure it doesn't go out of bounds
 	    yDeviation = random_integer(gen, 100) - 50;
 	    xDeviation = random_integer(gen, 100) - 50;
@@ -92,10 +96,12 @@ int main(int argc, char **argv) {
 	    	 (yDeviation + prevPosYD) > FIELD_SIZE_Y or (xDeviation + prevPosXD) > FIELD_SIZE_X);
 	  
 	  //calculate the versor which points in said direction
-	  dog[i].calcVersor(xDeviation + prevPosXD, yDeviation + prevPosYD);	  
-	  std::cout << "next pos: ("<< xDeviation + prevPosXD  <<"," << yDeviation + prevPosYD << ")" << std::endl;
-	  std::cout << "versor: ("<< *(dog[i].getVersor().begin()) <<"," 
-		    << *(dog[i].getVersor().begin()+1)<< ")\n" << std::endl;
+	  dog[i].calcVersor(xDeviation + prevPosXD, yDeviation + prevPosYD);
+	  if(verbose){
+	    std::cout << "next pos: ("<< xDeviation + prevPosXD  <<"," << yDeviation + prevPosYD << ")" << std::endl;
+	    std::cout << "versor: ("<< *(dog[i].getVersor().begin()) <<"," 
+		      << *(dog[i].getVersor().begin()+1)<< ")\n" << std::endl;
+	  }
 	}
 	else{
 	  dog[i].calcVersor(*(dog[i].getPosition().begin()+1)+DELTA_X*std::cos((prevPosYD + timeStep*SPEED_NODES)/8), 
@@ -108,21 +114,24 @@ int main(int argc, char **argv) {
 			     dogSpeed);
 
       }
+    
       else{ //isAstray == true
 	
 	if(!dog[i].isReturning){
-	  //when the dog is whithin 2 (m) from the firefighter, is no longer astray
-	  if(i==0)std::cout << "debug: " << std::sqrt(std::pow(*(dog[i].getNextPosition().begin()) - prevPosXD,2) + 
-		std::pow(*(dog[i].getNextPosition().begin()+1) - prevPosYD,2)) << std::endl;
+// 	  if(i==5){std::cout << "debug: " << std::sqrt(std::pow(*(dog[i].getNextPosition().begin()) - prevPosXD,2) + 
+// 		std::pow(*(dog[i].getNextPosition().begin()+1) - prevPosYD,2)) << std::endl;}
+	  //std::cout << timeStep * DOG_SPEED + STD_DEVIATION << std::endl;}
 	  if(std::sqrt(std::pow(*(dog[i].getNextPosition().begin()) - prevPosXD,2) + 
-		std::pow(*(dog[i].getNextPosition().begin()+1) - prevPosYD,2)) > 2){
-	    dog[i].setPosition(time, prevPosXD + *(dog[i].getVersor().begin())*timeStep*dogSpeed,
-				prevPosYD + *(dog[i].getVersor().begin()+1)*timeStep*dogSpeed,
-				dogSpeed);
+		std::pow(*(dog[i].getNextPosition().begin()+1) - prevPosYD,2)) > timeStep * DOG_SPEED + STD_DEVIATION){
+	    dog[i].setPosition(time, 
+			       prevPosXD + *(dog[i].getVersor().begin())*timeStep*dogSpeed,
+			       prevPosYD + *(dog[i].getVersor().begin()+1)*timeStep*dogSpeed,
+			       dogSpeed);
 	  }
 	  else{
 	    //TODO: Add pause();	  
-	    std::cout << "@" << time << "s dog[" << i <<"] reached goal destination " << std::endl;
+	    if(verbose)
+	      std::cout << "@" << time << "s dog[" << i <<"] reached goal destination " << std::endl;
 	    dog[i].isReturning = true;
 	    dog[i].setPosition(time, prevPosXD + *(dog[i].getVersor().begin())*timeStep*dogSpeed,
 				  prevPosYD + *(dog[i].getVersor().begin()+1)*timeStep*dogSpeed,
@@ -139,8 +148,10 @@ int main(int argc, char **argv) {
 			
 	  if(std::sqrt(std::pow(prevPosXD - prevPosXFF,2) + 
 		       std::pow(prevPosYD - prevPosYFF,2)) < 2){
-	    std::cout  << "@" << time << "s dog[" << i <<"] has returned to firefighter" << std::endl;
+	    if(verbose)
+	      std::cout  << "@" << time << "s dog[" << i <<"] has returned to firefighter" << std::endl;
 	    dog[i].isAstray = false;
+	    dog[i].isReturning = false;
 	  }
 	}
 	
@@ -160,31 +171,31 @@ int main(int argc, char **argv) {
       resultsFile << std::endl;
     }
 
-    for(int j = 0; j < NUM_NODES*2; j++){ //circle through nodes, all of them
-      for(int i = 0; i < firefighter[j].getPosition().size(); i+=4){ //circle through node's vectorPosition content
-	if(j<NUM_NODES){ //dogs
-	  if(i == 0){ //write initial position
-	    nsFile << "$node_(" << j << ") set X_ " << dog[j].getPosition().at(i+1) << std::endl;
-	    nsFile << "$node_(" << j << ") set Y_ " << dog[j].getPosition().at(i+2) << std::endl;
-	  }
-	  else{
-	    nsFile << "$ns_ at " << dog[j].getPosition().at(i) << " \"$node_(" << j << ") setdest " <<
-	      dog[j].getPosition().at(i+1) << " " << dog[j].getPosition().at(i+2) << " " << dog[j].getSpeed() << "\"" <<std::endl;
-	  }
-	}
-	else{ //firefighters
-	  if(i == 0){ //write initial position
-	    nsFile << "$node_(" << j << ") set X_ " << firefighter[j-NUM_NODES].getPosition().at(i+1) << std::endl;
-	    nsFile << "$node_(" << j << ") set Y_ " << firefighter[j-NUM_NODES].getPosition().at(i+2) << std::endl;
-	  }
-	  else{
-	    nsFile << "$ns_ at " << firefighter[j-NUM_NODES].getPosition().at(i) << " \"$node_(" << j << ") setdest " <<
-	      firefighter[j-NUM_NODES].getPosition().at(i+1) << " " << firefighter[j-NUM_NODES].getPosition().at(i+2) << " " << 
-	      firefighter[j-NUM_NODES].getSpeed() << "\"" <<std::endl;
-	  }
-	}
-      }
-    }
+//     for(int j = 0; j < NUM_NODES*2; j++){ //circle through nodes, all of them
+//       for(int i = 0; i < firefighter[j].getPosition().size(); i+=4){ //circle through node's vectorPosition content
+// 	if(j<NUM_NODES){ //dogs
+// 	  if(i == 0){ //write initial position
+// 	    nsFile << "$node_(" << j << ") set X_ " << dog[j].getPosition().at(i+1) << std::endl;
+// 	    nsFile << "$node_(" << j << ") set Y_ " << dog[j].getPosition().at(i+2) << std::endl;
+// 	  }
+// 	  else{
+// 	    nsFile << "$ns_ at " << dog[j].getPosition().at(i) << " \"$node_(" << j << ") setdest " <<
+// 	      dog[j].getPosition().at(i+1) << " " << dog[j].getPosition().at(i+2) << " " << dog[j].getPosition().at(i+3) << "\"" <<std::endl;
+// 	  }
+// 	}
+// 	else{ //firefighters
+// 	  if(i == 0){ //write initial position
+// 	    nsFile << "$node_(" << j << ") set X_ " << firefighter[j-NUM_NODES].getPosition().at(i+1) << std::endl;
+// 	    nsFile << "$node_(" << j << ") set Y_ " << firefighter[j-NUM_NODES].getPosition().at(i+2) << std::endl;
+// 	  }
+// 	  else{
+// 	    nsFile << "$ns_ at " << firefighter[j-NUM_NODES].getPosition().at(i) << " \"$node_(" << j << ") setdest " <<
+// 	      firefighter[j-NUM_NODES].getPosition().at(i+1) << " " << firefighter[j-NUM_NODES].getPosition().at(i+2) << " " << 
+// 	      firefighter[j-NUM_NODES].getSpeed() << "\"" <<std::endl;
+// 	  }
+// 	}
+//       }
+//     }
 	
     resultsFile.close();
     nsFile.close();
--- mobilityGenerator/parameters.h
+++ mobilityGenerator/parameters.h
@@ -5,23 +5,214 @@
 //TODO: set some more realistic parameters and maybe change FIELD_SIZE_COORDS to DISTANCE_BETWEEN_NODES
 //TODO: replace DOG_SPEED with a random number. It may be a good idea to draw speed from a normal distribution
 //	with a mean around firefighter_speed
-#define FIELD_SIZE_X 300
-#define FIELD_SIZE_Y 600
-#define NUM_NODES 6 //actually 6 firefighters and 6 dogs, so 12
-#define SPEED_NODES 0.4 //m/s
-#define STARTING_POS_X 100
-#define STARTING_POS_Y 100
-#define SAMPLES_NUMBER 500
-#define DURATION 900
-#define DELTA_X 15 //(m) this is the distance the dogs will travel before returning
-#define DELTA_Y 5 // how many meters are the dogs ahead of the firefighter
+//#define FIELD_SIZE_X 300
+//#define FIELD_SIZE_Y 600
+//#define NUM_NODES 6 //actually 6 firefighters and 6 dogs, so 12
+//#define SPEED_NODES 0.4 //m/s
+// #define STARTING_POS_X 100
+// #define STARTING_POS_Y 100
+//#define SAMPLES_NUMBER 500
+//#define DURATION 900
+//#define DELTA_X 15 //(m) this is the distance the dogs will travel before returning
+//#define DELTA_Y 5 // how many meters are the dogs ahead of the firefighter
 #define SINE_ARC_LENGTH 7.64
 #define PI 3.14159265
-#define FILEPATH "../../data/scenario10.movements" //FIXME
-#define FILEPATH1 "../../data/ns_scenario10.movements"
-#define DOG_SPEED (SINE_ARC_LENGTH*DELTA_X*SPEED_NODES)/(16*PI)
-#define STD_DEVIATION DOG_SPEED/2
-#define PROB	5 //in per mil
-#define SEED 0//to make it truly random set to 0
+#define FILEPATH "../../data/scenario11.movements" //FIXME
+#define FILEPATH1 "../../data/ns_scenario11.movements"
+//#define DOG_SPEED (SINE_ARC_LENGTH*DELTA_X*SPEED_NODES)/(16*PI)
+//#define STD_DEVIATION DOG_SPEED/2
+//#define PROB	5 //in per mil
+//#define SEED 0//to make it truly random set to 0
+#include "anyoption.cpp" //change to .h
+
+unsigned long SAMPLES_NUMBER, DELTA_X, DELTA_Y, DURATION, SEED, FIELD_SIZE_X, FIELD_SIZE_Y;
+unsigned int PROB, NUM_NODES;
+double STD_DEVIATION, SPEED_NODES, DOG_SPEED;
+bool verbose;
+std::string FILE_NAME;
+
+void askForParameters( int argc, char **argv){
+ 
+    /* 1. CREATE AN OBJECT */
+    //AnyOption *opt = new AnyOption();
+    AnyOption cmd;
+    /* 2. SET PREFERENCES  */
+    //cmd.noPOSIX(); /* do not check for POSIX style character options */
+    cmd.setVerbose(); /* print warnings about unknown options */
+    cmd.autoUsagePrint(true); /* print usage for bad options */
+
+    /* 3. SET THE USAGE/HELP   */
+    cmd.addUsage( "" );
+    cmd.addUsage( "Usage: " );
+    cmd.addUsage( "" );
+    cmd.addUsage( " -h   --help\t\tprints this help ");
+    cmd.addUsage( " -f   --file		file name");
+    cmd.addUsage( " -n   --nodes		number of nodes");
+    cmd.addUsage( " -s   --samples		numbers of samples to be taken per node ");
+    cmd.addUsage( " -d   --duration         the time during which something continues (in seconds)");
+    cmd.addUsage( " -x   --xLimit           limit of the y coordinate (in meters) ");
+    cmd.addUsage( " -y   --yLimit  	        limit of the y coordinate (in meters) ");
+    cmd.addUsage( " -p   --probability	probability of a node of deviating (in per mil)");
+    cmd.addUsage( " -D   --deviation	standard deviation of the probability distribution wich controlls the dogs' speed");
+    cmd.addUsage( " -r   --speed	speed of the firefighters");
+    cmd.addUsage( " -X   --deltaX		amplitude of the sine wave in meters (in meters)");
+    cmd.addUsage( " -Y   --deltaY		offset between the firefighters and the dogs (in meters)");
+    cmd.addUsage( " -S   --seed		seed, if set to 0 it will be random");
+    cmd.addUsage( " -v   --verbose	displays debug info");
+    cmd.addUsage( "" );
+
+    /* 4. SET THE OPTION STRINGS/CHARACTERS */
+
+    /* by default all  options  will be checked on the command line and from option/resource file */
+    cmd.setFlag(  "help", 'h' );   /* a flag (takes no argument), supporting long and short form */ 
+    cmd.setOption(  "file", 'f' ); 
+    cmd.setOption(  "nodes", 'n' ); 
+    cmd.setOption(  "samples", 's' ); 
+    cmd.setOption(  "yLimit", 'y' ); 
+    cmd.setOption(  "xLimit", 'x' );
+    cmd.setOption(  "duration", 'd' ); 
+    cmd.setOption(  "probability", 'p' );
+    cmd.setOption(  "deviation", 'D' ); 
+    cmd.setOption(  "deltaX", 'X' ); 
+    cmd.setOption(  "deltaY", 'Y' ); 
+    cmd.setOption(  "seed", 'S' ); 
+    cmd.setFlag(  "verbose", 'v' );   /* a flag (takes no argument), supporting long and short form */ 
+    
+    /* for options that will be checked only on the command and line not in option/resource file */
+    cmd.setCommandFlag(  "zip" , 'z'); /* a flag (takes no argument), supporting long and short form */
+
+    /* for options that will be checked only from the option/resource file */
+    cmd.setFileOption(  "title" ); /* an option (takes an argument), supporting only long form */
+
+    /* 5. PROCESS THE COMMANDLINE AND RESOURCE FILE */
+
+    /* read options from a  option/resource file with ':' separated opttions or flags, one per line */
+    cmd.processFile( "/home/user/.options" );  
+    /* go through the command line and get the options  */
+    cmd.processCommandArgs( argc, argv );
+
+    if( ! cmd.hasOptions()) { /* print usage if no options */
+	    cmd.printUsage();
+	    return;
+    }
+
+    /* 6. GET THE VALUES */
+    if( cmd.getFlag( "help" ) || cmd.getFlag( 'h' ) ) 
+	cmd.printUsage();
+    
+    if( cmd.getFlag( "verbose" ) || cmd.getFlag( 'v' ) )
+	verbose = true;
+    
+    if( cmd.getValue( 's' ) != NULL  || cmd.getValue( "samples" ) != NULL  ){
+      SAMPLES_NUMBER = atoi(cmd.getValue('s')); 
+    }
+    else{
+      std::cout << "REQUIRED: number of samples" << std::endl;
+      std::cout << endl ;
+      std::terminate();   
+    }
+    
+    if( cmd.getValue( 'y' ) != NULL  || cmd.getValue( "yLimit" ) != NULL  ){
+      NUM_NODES = atoi(cmd.getValue('y')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: yLimit" << std::endl;
+      FIELD_SIZE_Y = 1000;
+    }
+    
+    if( cmd.getValue( 'x' ) != NULL  || cmd.getValue( "xLimit" ) != NULL  ){
+      SAMPLES_NUMBER = atoi(cmd.getValue('x')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: xLimit" << std::endl;
+      FIELD_SIZE_X = 1000;
+    }
+    
+    if( cmd.getValue( 'n' ) != NULL  || cmd.getValue( "nodes" ) != NULL  ){
+      NUM_NODES = atoi(cmd.getValue('n')); 
+    }
+    else{
+      std::cout << "REQUIRED: number of nodes" << std::endl;
+      std::cout << endl ;
+      std::terminate();
+    }
+   
+    if( cmd.getValue( 'r' ) != NULL  || cmd.getValue( "speed" ) != NULL  ){
+      NUM_NODES = atof(cmd.getValue('r')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: speed" << std::endl;
+      SPEED_NODES = 0.4;      
+    }
+    
+    if( cmd.getValue( 'd' ) != NULL  || cmd.getValue( "duration" ) != NULL  ){
+      DURATION = atoi(cmd.getValue('d')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: duration" << std::endl;
+      DURATION = 1000;     
+    }
+    
+    if( cmd.getValue( 'p' ) != NULL  || cmd.getValue( "probability" ) != NULL  ){
+      DURATION = atoi(cmd.getValue('p')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: probability" << std::endl;
+      PROB = 5;     
+    }
+    
+        
+    if( cmd.getValue( 'X' ) != NULL  || cmd.getValue( "deltaX" ) != NULL  ){
+      DURATION = atoi(cmd.getValue('X')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: deltaX" << std::endl;
+      DELTA_X = FIELD_SIZE_X / (NUM_NODES * 2);     
+    }
+           
+    if( cmd.getValue( 'Y' ) != NULL  || cmd.getValue( "deltaY" ) != NULL  ){
+      DURATION = atoi(cmd.getValue('Y')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: deltaY" << std::endl;
+      DELTA_Y = 5;     
+    }
+    
+    if( cmd.getValue( 'f' ) != NULL  || cmd.getValue( "file" ) != NULL  ){
+      FILE_NAME = (cmd.getValue('f')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: file name" << std::endl;
+      FILE_NAME = "scenario" ;     
+    }
+    
+    if( cmd.getValue( 'S' ) != NULL  || cmd.getValue( "seed" ) != NULL  ){
+      SEED = atoi(cmd.getValue('S')); 
+    }
+    else{
+      std::cout << "no SEED specified, using random" << std::endl;
+      SEED = 0;     
+    }
+
+     DOG_SPEED = (SINE_ARC_LENGTH*DELTA_X*SPEED_NODES)/(16*PI);//16PI is the distance travel by the ff while cos(y/8)
+     //TODO: DOG_SPEED*timeStep is the minumm sensibility for the return to firefighter
+     //According to the internet an average speed would be 11 m/s, that is running. But do the dogs run or walk or gallop or?
+     std::cout << "Average speed of dogs: " << DOG_SPEED << "m/s. Please check that this is a sane value." << endl;
+     
+    if( cmd.getValue( 'D' ) != NULL  || cmd.getValue( "deviation" ) != NULL  ){
+      DURATION = atof(cmd.getValue('D')); 
+    }
+    else{
+      std::cout << "USING DEFAULT: deviation" << std::endl;
+      STD_DEVIATION = DOG_SPEED/2;     
+    }
+     
+     std::cout << endl ;
+
+    /* 8. DONE */
+    //delete opt;
+ 
+}
+
 
 #endif//PARAMETERS.H
